c301f2e32daff3e594e3da4e192fb2620d815e5a
AARSHI <aarora0301@gmail.com>
2019-03-16 12:57:25 +0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Person from './Person/Person';
 class App extends Component {
 
     state = {
-
         persons: [
             {name: 'aarshi', age: 28},
             {name: 'anu', age: 27}
         ]
+    }
+
+    switchNameHandler=()=>{
+    console.log("clicked");
     }
 
     render() {
@@ -17,6 +20,7 @@ class App extends Component {
             <div className="App">
                 <h1>Hello React</h1>
                 <p>This is really Working!!</p>
+                <button onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Racing</Person>
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}></Person>
             </div>
